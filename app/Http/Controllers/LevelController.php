98c06a9fbe97c5d223c761de006cc7fe31591db6
AlQoronii <fathurrhazaq@gmail.com>
2024-03-03 16:20:34 +0700

    Praktikum 4 - Implementasi DB Facede

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LevelController extends Controller
+{
+    public function index(){
+        // Insert Data
+        // DB::insert('insert into m_levels(level_kode, level_nama, created_at) values(?,?,?)', ['CUS', 'Pelanggan', now()]);
+        //return 'Insert Data Baru Berhasil!!!';
+
+        // Update Data
+        //$row = DB::update ('update m_levels set level_nama = ? where level_kode = ?', ['customer', 'CUS']);
+        //return 'Update Data Berhasil!!!. Jumlah data yang diupdate: ' . $row . 'baris';
+        
+        // Delete Data
+        //$row = DB::delete('delete from m_levels where level_kode = ?', ['CUS']);
+        //return 'Delete Data Berhasil!!!. Jumlah data yang dihapus: ' . $row . ' Baris';
+    
+        // Select Data
+        $data = DB::select('select * from m_levels');
+        return view('level', ['data' => $data]);
+    }
+}
