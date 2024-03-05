70ca36f2ed87ffa34ccefb92e662b94d2e9d99dc
AlQoronii <fathurrhazaq@gmail.com>
2024-03-05 12:50:03 +0700

    Praktikum 2.5 - Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -114,5 +114,51 @@ class UserController extends Controller
 
         // return view('user', ['data' => $user]);
 
+        //Praktikum 2.5 
+        //Langkah 1
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+
+        // $user->username = 'manager55';
+
+        // $user->isDirty();
+        // $user->isDirty('username');
+        // $user->isDirty('nama');
+        // $user->isDirty(['nama','username']);
+
+        // $user->isClean();
+        // $user->isClean('username');
+        // $user->isClean('nama');
+        // $user->isClean(['nama','username']);
+
+        // $user->save();
+
+        // $user->isDirty();
+        // $user->isClean();
+        // dd($user->isDirty());
+
+
+        //Langkah 3
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['usename', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama','username']));
+
     }
 }
