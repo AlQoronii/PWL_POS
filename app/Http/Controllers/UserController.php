ddaeac6b033c60bc4c08bf2f6dfeed0182580776
AlQoronii <fathurrhazaq@gmail.com>
2024-03-05 10:27:10 +0700

    Praktikum 1 dan Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,12 +22,42 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
 
 
-        $data =[
-            'nama'=> 'Pelanggan Pertama',
-        ];
-        UserModel::where('username','customer-1')->update($data);
+        // $data =[
+        //     'nama'=> 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username','customer-1')->update($data);
 
-        $user = UserModel::all();
-        return view('user',['data' => $user]);
+        // $user = UserModel::all();
+        // return view('user',['data' => $user]);
+            
+        // $data =[
+        //     'level_id'=>2,
+        //     'username'=>'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')  
+        // ];
+        // UserModel::create($data);
+
+        // $user = UserModel::all();
+        // return view('user',['data'=>$user]);
+
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id',1)->first();
+        // return view('user', ['data'=>$user]);
+
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data'=>$user]);
+
+        // $user = UserModel::findOr(1,['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data'=>$user]);
+        
+        $user = UserModel::findOr(20,['username', 'nama'], function(){
+            abort(404);
+        });
+        return view('user', ['data'=>$user]);
     }
 }
