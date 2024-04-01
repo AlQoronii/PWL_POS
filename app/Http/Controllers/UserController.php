b663fe82eefcf8c3bfcd0f064bdf2c51410eddf6
AlQoronii <fathurrhazaq@gmail.com>
2024-04-01 21:30:52 +0700

    Praktikum 4 - Implementasi Filtering DataTables

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -227,13 +227,20 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // Set menu yang sedang aktif
 
-        return view('user.index',['breadcrumb'=>$breadcrumb,'page'=>$page, 'activeMenu'=>$activeMenu]);
+        $level = LevelModel::all(); // ambil data level untuk filter level
+        return view('user.index',['breadcrumb'=>$breadcrumb,'page'=>$page,'level'=>$level, 'activeMenu'=>$activeMenu]);
     }
 
     public function list(Request $request){
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
-                    ->with('level')
-                    ->get();
+                    ->with('level');
+                    
+        // Filter data user bedasarkan level_id
+        if ($request->level_id){
+            $users->where('level_id',$request->level_id);
+        }
+
+                    
 
         return DataTables::of($users)
             ->addIndexColumn() // Menambahkan kolom index / no urut (default nmaa kolom: DT_RowINdex)
